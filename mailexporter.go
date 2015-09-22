8b23971ff7508b9f15529282177484085a6a02e3
Jonas Große Sundrup <cherti@letopolis.de>
2015-09-22 23:27:18 +0200

    style-overhaul

--- mailexporter.go
+++ mailexporter.go
@@ -22,11 +22,10 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-var globalconf config
 var contentLength = 40 // length of payload for probing-mails
 
 // holds a configuration of external server to send test mails
-type config struct {
+var globalconf struct {
 	CrtPath      string
 	KeyPath      string
 	AuthUser     string
@@ -35,32 +34,34 @@ type config struct {
 	HTTPEndpoint string
 
 	MonitoringInterval time.Duration
-	StartupOffset time.Duration
-	MailCheckTimeout  time.Duration
+	StartupOffset      time.Duration
+	MailCheckTimeout   time.Duration
 
 	Servers []SMTPServerConfig
 	//Servers []map[string]string
 }
 
 type SMTPServerConfig struct {
-	Name string
-	Server string
-	Port string
-	Login string
-	Passphrase string
-	From string
-	To string
+	Name         string
+	Server       string
+	Port         string
+	Login        string
+	Passphrase   string
+	From         string
+	To           string
 	Detectiondir string
 }
 
-// cli-flags
-var confPath = flag.String("config-file", "./mailexporter.conf", "config-file to use")
-var useTLS = flag.Bool("tls", true, "use TLS for metrics-endpoint")
-var useAuth = flag.Bool("auth", true, "use HTTP-Basic-Auth for metrics-endpoint")
+var (
+	// cli-flags
+	confPath = flag.String("config-file", "./mailexporter.conf", "config-file to use")
+	useTLS   = flag.Bool("tls", true, "use TLS for metrics-endpoint")
+	useAuth  = flag.Bool("auth", true, "use HTTP-Basic-Auth for metrics-endpoint")
 
-// errors
-var ErrMailNotFound = errors.New("no corresponding mail found")
-var ErrNotOurDept = errors.New("no mail of ours")
+	// errors
+	ErrMailNotFound = errors.New("no corresponding mail found")
+	ErrNotOurDept   = errors.New("no mail of ours")
+)
 
 // holds information about probing-email with the corresponding file name
 type email struct {
@@ -77,45 +78,51 @@ var deliver_ok = prometheus.NewGaugeVec(
 		Name: "mail_deliver_success",
 		Help: "indicatior whether last mail was delivered successfully",
 	},
-	[]string{"configname"})
+	[]string{"configname"},
+)
 
 var last_mail_deliver_time = prometheus.NewGaugeVec(
 	prometheus.GaugeOpts{
 		Name: "last_mail_deliver_time",
 		Help: "timestamp (in s) of detection of last correctly received testmail",
 	},
-	[]string{"configname"})
+	[]string{"configname"},
+)
 
 var last_mail_deliver_duration = prometheus.NewGaugeVec(
 	prometheus.GaugeOpts{
 		Name: "last_mail_deliver_duration",
 		Help: "duration (in ms) of delivery of last correctly received testmail",
 	},
-	[]string{"configname"})
+	[]string{"configname"},
+)
 
 var late_mails = prometheus.NewCounterVec(
 	prometheus.CounterOpts{
 		Name: "late_mails",
 		Help: "number of probing-mails received after their respective timeout",
 	},
-	[]string{"configname"})
+	[]string{"configname"},
+)
 
 var mail_deliver_durations = prometheus.NewHistogramVec(
 	prometheus.HistogramOpts{
-		Name: "mail_deliver_durations",
-		Help: "durations (in ms) of mail delivery",
+		Name:    "mail_deliver_durations",
+		Help:    "durations (in ms) of mail delivery",
 		Buckets: histBuckets(100e3, 50),
 	},
-	[]string{"configname"})
+	[]string{"configname"},
+)
 
-func histBuckets(upperBound float64, binsize float64) []float64 {
-	bins := int(upperBound)/int(binsize)
+// histBuckets returns a linearly spaced []float64 to be used as Buckets in a prometheus.Histogram.
+func histBuckets(upperBound float64, binSize float64) []float64 {
+	bins := int(upperBound) / int(binSize)
 
 	buckets := make([]float64, bins)
-	binborder := binsize
+	binBorder := binSize
 	for i := 0; i < bins; i++ {
-		buckets[i] = binborder
-		binborder += binsize
+		buckets[i] = binBorder
+		binBorder += binSize
 	}
 	return buckets
 }
@@ -128,17 +135,14 @@ func init() {
 	prometheus.MustRegister(mail_deliver_durations)
 }
 
-// parse configuration file and make sure we are ready to rumble
+// parseConfig parses configuration file and tells us if we are ready to rumble.
 func parseConfig(path string) error {
-
 	content, err := ioutil.ReadFile(path)
-
 	if err != nil {
 		return err
 	}
 
 	err = yaml.Unmarshal(content, &globalconf)
-
 	if err != nil {
 		return err
 	}
@@ -146,28 +150,28 @@ func parseConfig(path string) error {
 	return nil
 }
 
-// send email over SMTP-server specified in config c
+// send sends a probing-email over SMTP-server specified in config c to be waited for on the receiving side.
 func send(c SMTPServerConfig, msg string) {
-
 	//fmt.Println("sending mail")
 	a := smtp.PlainAuth("", c.Login, c.Passphrase, c.Server)
 	err := smtp.SendMail(c.Server+":"+c.Port, a, c.From, []string{c.To}, []byte(msg))
 
 	if err != nil {
 		fmt.Println(err)
-		fmt.Println(c.Server+":"+c.Port)
+		fmt.Println(c.Server + ":" + c.Port)
 	}
 }
 
-// returns a random string to pad the send mail with for identifying
+// randString returns a random string to pad the send mail with for identifying
 // it later in the maildir (and not mistake another one for it)
 // does also return unprintable characters in returned string,
 // which is actually appreciated to implicitly monitor that
 // mail gets through unchanged
 // although, if you would print those unprintable characters,
 // be aware of funny side-effects like terminal commands being
-// triggered and stuff like that
-func randstring(length int) string {
+// triggered and stuff like that, therefore use
+// fmt.Printf("%q", unprintableString) for that
+func randString(length int) string {
 
 	stuff := make([]byte, length)
 
@@ -189,34 +193,32 @@ func randstring(length int) string {
 	return rstr
 }
 
-// delete the given mail to not leave an untidied maildir
-func delmail(m email) {
-	err := os.Remove(m.Filename)
-	if err != nil {
+// deleteMail delete the given mail to not leave an untidied maildir.
+func deleteMail(m email) {
+	if err := os.Remove(m.Filename); err != nil {
 		fmt.Println(err)
 	}
 	//fmt.Println("rm ", m.Filename)
 }
 
-// compose a payload to be used in probing mails for identification
+// composePayload composes a payload to be used in probing mails for identification
 // consisting of config name, unix time and padding of appropriate length
-func composePayload(name string, unixtimestamp int64) (string, string) {
+func composePayload(name string, unixtimestamp int64) (payload string, token string) {
 	timestampstr := strconv.FormatInt(unixtimestamp, 10)
 	remainingLength := contentLength - len(name) - len(timestampstr) - 2 // 2 for two delimiters
 
 	// now get the token to have a unique token and use it to pad to full contentLength
-	token := randstring(remainingLength)
+	token = randString(remainingLength)
 
-	payload := name + "-" + token + "-" + timestampstr
+	payload = name + "-" + token + "-" + timestampstr
 	//fmt.Println("composed payload:", payload)
 
 	return payload, token
 }
 
-// return the config name and unix timestamp as appropriate types
+// decomposePayload return the config name and unix timestamp as appropriate types
 // from given payload
-func decomposePayload(payload []byte) (string, string, int64, error) {
-
+func decomposePayload(payload []byte) (name string, token string, extractedUnixTime int64, err error) {
 	// is the length correct?
 	if len(payload) != contentLength {
 		return "", "", -1, ErrNotOurDept
@@ -225,43 +227,43 @@ func decomposePayload(payload []byte) (string, string, int64, error) {
 	//fmt.Println("payload to decompose:", payload)
 
 	decomp := strings.Split(string(payload), "-")
-
 	// is it correctly parsable?
 	if len(decomp) != 3 {
 		//fmt.Println("no fitting decomp")
 		return "", "", -1, ErrNotOurDept
 	}
 
-	extractedUnixtime, err := strconv.ParseInt(decomp[2], 10, 64)
-
+	extractedUnixTime, err = strconv.ParseInt(decomp[2], 10, 64)
 	// is the last one a unix-timestamp?
 	if err != nil {
 		//fmt.Println("unix-timestamp-parse-error")
 		return "", "", -1, ErrNotOurDept
 	}
 
-	return decomp[0], decomp[1], extractedUnixtime, nil
+	return decomp[0], decomp[1], extractedUnixTime, nil
 }
 
-// log mails that have been so late that they timed out
+// lateMail logs mails that have been so late that they timed out
 func lateMail(m email) {
 	//fmt.Println("got late mail via", m.Name)
 	late_mails.WithLabelValues(m.Name).Inc()
 }
 
-// probe if mail gets through the entire chain from specified SMTP into Maildir
+// probe probes if mail gets through the entire chain from specified SMTPServer into Maildir.
+// the argument "reportChans" contains channels to each monitoring goroutine where to drop
+// the found mails into.
 func probe(c SMTPServerConfig, reportChans map[string]chan email) {
-
 	payload, token := composePayload(c.Name, time.Now().UnixNano())
 	send(c, payload)
 
 	timeout := time.After(globalconf.MailCheckTimeout)
 
-	// "for seekingMail" is needed to account for mails that are coming late
-	// otherwise, a late mail would trigger the first case and stop us from
+	// "for seekingMail" is needed to account for mails that are coming late.
+	// Otherwise, a late mail would trigger the first case and stop us from
 	// being able to detect the mail we are actually waiting for
-	seekingMail := true
-	for seekingMail {
+
+seekloop:
+	for {
 		select {
 		case mail := <-reportChans[c.Name]:
 			//fmt.Println("getting mail...")
@@ -269,8 +271,8 @@ func probe(c SMTPServerConfig, reportChans map[string]chan email) {
 			// timestamps are in nanoseconds
 			// last_mail_deliver_time shall be standard unix-timestamp
 			// last_mail_deliver_duration shall be milliseconds for higher resolution
-			deliverTime := float64(mail.T_recv/int64(time.Second))
-			deliverDuration := float64((mail.T_recv - mail.T_sent)/int64(time.Millisecond))
+			deliverTime := float64(mail.T_recv / int64(time.Second))
+			deliverDuration := float64((mail.T_recv - mail.T_sent) / int64(time.Millisecond))
 			last_mail_deliver_time.WithLabelValues(c.Name).Set(deliverTime)
 			last_mail_deliver_duration.WithLabelValues(c.Name).Set(deliverDuration)
 			mail_deliver_durations.WithLabelValues(c.Name).Observe(deliverDuration)
@@ -278,34 +280,32 @@ func probe(c SMTPServerConfig, reportChans map[string]chan email) {
 			if mail.Token == token {
 				// we obtained the expected mail
 				deliver_ok.WithLabelValues(c.Name).Set(1)
-				delmail(mail)
-				seekingMail = false
+				deleteMail(mail)
+				break seekloop
 
-			} else {
-				// it was another mail with unfitting token, probably late
-				lateMail(mail)
 			}
+			// it was another mail with unfitting token, probably late
+			lateMail(mail)
 
 		case <-timeout:
 			//fmt.Println("getting mail timed out")
 			deliver_ok.WithLabelValues(c.Name).Set(0)
-			seekingMail = false
+			break seekloop
 		}
-
 	}
 }
 
-// probe every couple of δt if mail still gets through
+// monitor probes every $(globalconf.MonitoringInterval) if mail still gets through.
 func monitor(c SMTPServerConfig, wg *sync.WaitGroup, reportChans map[string]chan email) {
 	fmt.Println("started monitoring for config", c.Name)
 	for {
 		probe(c, reportChans)
 		time.Sleep(globalconf.MonitoringInterval)
 	}
-	wg.Done()
+	//wg.Done()
 }
 
-// return secret for basic http-auth
+// Secret returns secret for basic http-auth
 func Secret(user, realm string) string {
 	if user == globalconf.AuthUser {
 		return globalconf.AuthPass
@@ -313,9 +313,8 @@ func Secret(user, realm string) string {
 	return ""
 }
 
-// monitor Detectiondirs and report mails that come in
+// detectMail monitors Detectiondirs and reports mails that come in.
 func detectMail(watcher *fsnotify.Watcher, reportChans map[string]chan email) {
-
 	fmt.Println("started mail-detection")
 
 	for {
@@ -332,12 +331,10 @@ func detectMail(watcher *fsnotify.Watcher, reportChans map[string]chan email) {
 			fmt.Println("watcher-error:", err)
 		}
 	}
-
 }
 
-// read a mailfile's content and parse it into a mail-struct if one of ours
+// parseMail reads a mailfile's content and parses it into a mail-struct if one of ours.
 func parseMail(path string) (email, error) {
-
 	// to date the mails found
 	t := time.Now().UnixNano()
 
@@ -349,21 +346,17 @@ func parseMail(path string) (email, error) {
 	mail.Body.Read(payload)
 
 	name, token, unixtime, err := decomposePayload(payload)
-
 	// return if parsable
 	// (non-parsable mails are not sent by us (or broken) and therefore not needed
-	if err == nil {
-		return email{path, name, token, unixtime, t}, nil
-	} else {
+	if err != nil {
 		return email{}, ErrNotOurDept
 	}
 
+	return email{path, name, token, unixtime, t}, nil
 }
 
 func main() {
-
 	flag.Parse()
-
 
 	// seed the RNG, otherwise we would have same randomness on every startup
 	// which should not, but might in worst case interfere with leftover-mails
@@ -371,7 +364,6 @@ func main() {
 	rand.Seed(time.Now().Unix())
 
 	err := parseConfig(*confPath)
-
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -382,9 +374,7 @@ func main() {
 		late_mails.GetMetricWithLabelValues(c.Name)
 	}
 
-
 	fswatcher, err := fsnotify.NewWatcher()
-
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -392,7 +382,9 @@ func main() {
 
 	defer fswatcher.Close()
 
+	// reportChans will be used to send found mails from the detection-goroutine to the monitoring-goroutines
 	reportChans := make(map[string]chan email)
+
 	for _, c := range globalconf.Servers {
 		fswatcher.Add(c.Detectiondir) // deduplication is done within fsnotify
 		reportChans[c.Name] = make(chan email)
@@ -434,5 +426,4 @@ func main() {
 	// wait for goroutines to exit
 	// otherwise main would terminate and the goroutines monitoring would be killed
 	wg.Wait()
-
 }
